8c351a491aaab2017df2b7c475f99eff859ae5a7
Étienne Deparis <etienne@depar.is>
2018-06-30 14:06:52 +0200

    feat: Support grammalecte 0.6.4+
    Fixes: https://gitlab.com/geeklhem/flycheck-grammalecte/issues/4

--- flycheck-grammalecte.py
+++ flycheck-grammalecte.py
@@ -13,32 +13,22 @@ any later version.
 """
 
 import fileinput
-
-import grammalecte.fr as gce
+import grammalecte
 import grammalecte.text as txt
-import grammalecte.tokenizer as tkz
 
 
 def main():
     """Read the file and run grammalecte on it"""
     # Load grammalecte.
-    gce.load()
-    dictionary = gce.getDictionary()
-    tokenizer = tkz.Tokenizer("fr")
+    oGrammarChecker = grammalecte.GrammarChecker("fr")
 
     # Read input from stdin or first arg.
     text_input = [line for line in fileinput.input()]
     text, lineset = txt.createParagraphWithLines(list(enumerate(text_input)))
 
     # Grammar errors
-    gramm_err = gce.parse(text, "FR", bDebug=False, bContext=True)
-
-    # Spelling errors
-    spell_err = []
-    for token in tokenizer.genTokens(text):
-        if token["sType"] == "WORD" and \
-           not dictionary.isValidToken(token["sValue"]):
-            spell_err.append(token)
+    gramm_err, spell_err = oGrammarChecker.getParagraphErrors(
+        text, bDebug=False)
 
     # Get colums and lines.
     gramm_err, spell_err = txt.convertToXY(gramm_err, spell_err, lineset)

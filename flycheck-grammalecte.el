2f4812eaa6e809051e23c34a085df2b704a15741
Étienne Deparis <etienne@depar.is>
2019-02-12 17:05:15 +0100

    chore: Bump version

--- flycheck-grammalecte.el
+++ flycheck-grammalecte.el
@@ -7,7 +7,7 @@
 ;; Author: Guilhem Doulcier <guilhem.doulcier@espci.fr>
 ;;         Étienne Deparis <etienne@depar.is>
 ;; Created: 21 February 2017
-;; Version: 0.5
+;; Version: 0.7
 ;; Package-Requires: ((emacs "24.3") (flycheck "26"))
 ;; Keywords: i18n, text
 ;; Homepage: https://git.deparis.io/flycheck-grammalecte/

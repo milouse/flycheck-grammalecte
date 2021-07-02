379f140b87e12a2f3f01df6ed6f55bb073d66f31
Étienne Deparis <etienne@depar.is>
2021-07-02 19:23:22 +0200

    tests: Enable debug-on-error and avoid poluting user directory in test-profile.el

--- test-profile.el
+++ test-profile.el
@@ -3,7 +3,11 @@
       visible-bell t
       warning-minimum-level :debug
       warning-minimum-log-level :debug
+      debug-on-error t
       org-startup-folded "showeverything"
+      grammalecte-settings-file (expand-file-name
+                                 "tmp-grammalecte-cache.el"
+                                 (file-name-directory load-file-name))
       grammalecte--debug-mode t)
 
 (with-eval-after-load 'flycheck
